781ed696874d2ddf56b4b459900b7476377391dc
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-23 14:10:07 +0200

    last changes

--- staff/mario-rubio/web-apps/hello-world/components/register.js
+++ staff/mario-rubio/web-apps/hello-world/components/register.js
@@ -1,5 +1,20 @@
 function mountRegister(container, onRegister) {
-    
+    var temp = document.createElement("div");
+
+    temp.innerHTML = `<section class="register">
+    <h2>Register</h2>
+
+    <form class="register__form">
+        <input type="text" name="fullname" placeholder="full name" _required>
+        <input type="email" name="email" placeholder="e-mail" required>
+        <input type="password" name="password" placeholder="password" required>
+        <input type="password" name="repassword" placeholder="repeat password" required>
+        <button>Register</button>
+    </form>
+</section>`
+
+    var container = temp.firstChild;
+
     var form = container.querySelector('.register__form');
 
     form.onsubmit = function (event) {
@@ -19,4 +34,5 @@ function mountRegister(container, onRegister) {
             alert(error.message);
         }
     }
+    return container;
 };
--- staff/mario-rubio/web-apps/hello-world/components/title.js
+++ staff/mario-rubio/web-apps/hello-world/components/title.js
@@ -1,7 +1,12 @@
-function mountTitle(selector, onHome) {
-    
-    var title =document.querySelector(selector);
-    
-    title.onclick = onHome;
+function mountTitle(onHome) {
+    var temp = document.createElement("div");
+
+    temp.innerHTML = '<h1 class="title">Hello World App</h1>'
+
+    var container = temp.firstChild;
+
+    container.onclick = onHome;
+
+    return container;
 
 };
