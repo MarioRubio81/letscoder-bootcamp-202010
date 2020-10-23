2faf0b47af2bc8542d74818250528e93ac6148bb
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-23 11:20:34 +0200

    adding

--- staff/mario-rubio/web-apps/hello-world/logic/unregister-user.demo.js
+++ staff/mario-rubio/web-apps/hello-world/logic/unregister-user.demo.js
@@ -1,1 +1,10 @@
-//TODO play here
+unregisterUser('0000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZjkyOGE4MjRlM2YxMDAwMTc0MTI2ZDQiLCJpYXQiOjE2MDM0MzkyNDQsImV4cCI6MTYwMzQ0Mjg0NH0.Dn7VIoazQsjEk8VT4FY1azBw8a-Ub0rXyMeMD9LdD-0', function(error) {
+    
+    console.log('DEMO unregisterUser()');
+
+    if (error) console.error(error)
+    else console.log("User deleted")
+})
+
+
+//OJO !!!! DA UN ERROR EXTRAÑO (HACER USUARIO PARA PODER EJECUTAR EN CONSOLA Y BORRAR)
